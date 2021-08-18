20042b6277c0321675bb74b6e56d82e24a6e0f19
kevinr2 <kevinreve096@gmail.com>
2021-08-18 18:43:45 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import {useEffect ,useState} from 'react'; 
 import './App.css';
+import { API } from './utils/Api';
+import Table from "./components/Table";
+//axios
+import axios from 'axios';
 
+const api = API;
+ 
 function App() {
+
+   const [coins, setCoins] =useState([]);
+   const [search, setSearch] =useState('');
+
+   const getData = async ()=>{
+    const resp = await axios.get(api)
+    setCoins(resp.data)
+   }
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <h3 className="text-center title">API Bitcoins</h3>
+      <div className="row">
+        <input type='text' placeholder="search..." className="form-control bg-dark text-light border-8 mt-4 text-center"
+          onChange={e=>setSearch(e.target.value)}
+        />
+        <Table coins={coins} search={search}/>
+      </div>
     </div>
   );
 }
